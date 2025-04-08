65af6498e5eb645f0f937732e1d37d33ee819a80
Lại Văn Hùng <laivanhung1412@gmail.com>
2025-04-08 23:49:22 +0700

    create service for chat and comment, create dto

---
+++ SocialMedia/src/main/java/com/example/SocialMedia/dto/request/CommentRequestDTO.java
@@ -0,0 +1,18 @@
+package com.example.SocialMedia.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentRequestDTO {
+
+    private String postId;
+    @NotNull
+    private String content;
+}
---
+++ SocialMedia/src/main/java/com/example/SocialMedia/dto/request/ReactionRequestDTO.java
@@ -0,0 +1,16 @@
+package com.example.SocialMedia.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReactionRequestDTO {
+    @NotNull
+    private String name;
+}
